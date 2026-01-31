0af543c204c33f954e6e5138308919eb53799d3e
Naeem Sajjad <naeem9924066@gmail.com>
2026-01-31 18:50:22 +0500

    added frontend url to the cors

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 const port = process.env.PORT|| 3000;
 app.use(cors({
-    origin:['http://localhost:5173','http://localhost:5174','http://localhost:3000'],
+    origin:['http://localhost:5173','http://localhost:5174','http://localhost:3000','https://thumblify-by-naeem.vercel.app'],
     credentials:true,
 
 }))
